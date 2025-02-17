b3e6b5906890ac07dcc8def2b78d2a6ee603f610
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-02-17 11:07:21 +0500

    first commit

---
+++ Class01/index.py
@@ -0,0 +1,45 @@
+# nm = input('What is your name : ');
+# print(f'hi {nm}');
+
+
+# num1 = 5
+# num2 = 5.5
+
+# print(num1 + num2)
+
+
+# sunny = False
+# rainy = True
+
+# if rainy and not sunny : 
+#   print('Its sunny day')
+
+
+# List 
+
+# fruits = ['ayaz','khan']
+# fruits.append('bro');
+# fruits.remove('ayaz');
+# print(len(fruits))
+# print(fruits)
+# print(fruits[0])
+
+
+# Tuple
+# # Tuple are immutable
+
+# cordinates = (10.5,11.6,12.7)
+# # cordinates[1] = 1 Error
+
+# print(cordinates[0])
+
+
+# Dictionary 
+
+stud = {
+  'name' : 'ayaz',
+  'major' : 'CS'
+}
+del stud['major']
+
+print(stud )
